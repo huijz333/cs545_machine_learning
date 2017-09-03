75cb7df5bac595867ab965d59d9fcc751f102ed6
Ben Wilson <btw@pdx.edu>
2017-09-02 22:10:42 -0700

    tweaked script

--- hw1_perceptron/python/main.py
+++ hw1_perceptron/python/main.py
@@ -6,10 +6,20 @@ import perceptron
 
 
 start = time.time()
+p = perceptron.Perceptron(alpha=0.0001,
+                          eta0=1.0,
+                          n_iter=10,
+                          grad_descent='stochastic',
+                          batch_size=1,
+                          max_iter=None,
+                          tol=None,
+                          shuffle=False,
+                          rand_state=True)
 
 images, labels = mnist.retrieve('training')
-# ind = 2
-# mnist.show_digit(images[ind], labels[ind])
+p.fit(images, labels)
+
+# mnist.show_digit(images[80], label='not 0')
 
 stop = time.time()
 print('time: {} seconds'.format(stop-start))
